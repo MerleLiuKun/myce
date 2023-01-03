29e49e11f4bb23f1f38189c66e4a2cae40a1fbee
ikaroskun <merle.liukun@gmail.com>
2023-01-03 15:12:07 +0800

    feat(limit): :sparkles: tests with timelimit

--- tasks/entry.py
+++ tasks/entry.py
@@ -3,6 +3,8 @@
 """
 import time
 
+from celery.exceptions import SoftTimeLimitExceeded, TimeLimitExceeded
+
 from celery_entry import celery_app
 from tasks import logger
 
@@ -27,3 +29,22 @@ def long_time_task_with_output(seconds=10):
             logger.info(f"Now sleep at seconds: {idx}")
         time.sleep(1)
     return True
+
+
+@celery_app.task(queue="test-queue", time_limit=10)
+def long_time_task_limit(seconds=20):
+    seconds = int(seconds)
+    try:
+        for idx in range(seconds):
+            if idx % 5 == 0:
+                logger.info(f"Now sleep at seconds: {idx}")
+            time.sleep(1)
+        return True
+    except SoftTimeLimitExceeded:
+        # Won't stop worker.
+        logger.info(f"Sorry, we have soft task limit to 10s.")
+        return False
+    except TimeLimitExceeded:
+        # Will stop worker and create new worker.
+        logger.info(f"Sorry, we have task limit to 10s.")
+        return False
